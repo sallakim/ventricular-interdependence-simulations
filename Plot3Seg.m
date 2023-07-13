bb4746a91161507794fbb85261f66f3244d1a3bb
Jezek <jezekf@gmail.com>
2023-07-13 18:55:56 +0200

    Attempt on thickness

--- Plot3Seg.m
+++ Plot3Seg.m
@@ -1,25 +1,55 @@
 % visualize the time course
 clear;
+% figure(101);clf; hold on;
+
 xy = load('TriSegAnim_normal.mat').xy;
 % xy = [t, xm_lv, xm_sep,  xm_rv, ym, t_lv, t_sep,  t_rv]; % data structure
 % format
-slides = [1:10:900];
+slides = [1, 2];
 t = xy(slides , 1);
 xm_lv = xy(slides , 2);
 xm_sep = xy(slides , 3);
 xm_rv = xy(slides , 4);
 ym = xy(slides ,5);
 
-% Circle Function For Angles In Degrees
-plotArc = @(radius,deg_ang)  [radius*cosd(deg_ang);  radius*sind(deg_ang)]; 
+Vw_lv = 82; % cm3
+Vw_sep = 41; % cm3
+Vw_rv = 26; % cm3
 
-Vw_lv = 8.2;
-Vw_sep = 4.1;
-Vw_rv = 2.6;
+% Calculation the wall thickness
+VmInner = @(h, xm, ym) 1/6*pi*(xm + h).*(3*ym.^2 + (xm + h).^2);
+VmOuter = @(h, xm, ym) 1/6*pi*(xm - h).*(3*ym.^2 + (xm - h).^2);
+% this should be zero, as Vw = VmOUter - VmInner;
+VwBalance = @(h, xm, ym, Vw) -VmOuter(h, xm, ym) + VmInner(h, xm, ym) - Vw; 
+i = 1;
+for i = 1:length(xm_lv)
+    h_lv(i) = fzero(@(h)VwBalance(h, xm_lv(i), ym(i), Vw_lv),  [-10 10]);
+    h_sep(i) = fzero(@(h)VwBalance(h, xm_sep(i), ym(i), Vw_lv),  [-10 10]);
+    h_rv(i) = fzero(@(h)VwBalance(h, xm_rv(i), ym(i), Vw_lv),  [-10 10]);
+end
 
-Cm_lv=2*xm_lv./max(xm_lv.^2 + ym.^2, 1e-6);
-Cm_sep=2*xm_sep./max(xm_sep.^2 + ym.^2, 1e-6);
-Cm_rv=2*xm_rv./max(xm_rv.^2 + ym.^2, 1e-6);
+% %add up to the thicknesses
+% % inner lining
+% xm_lv = xm_lv + h_lv;
+% xm_sep = xm_sep - h_sep;
+% xm_rv = xm_rv - h_rv;
+% % ym = ym - h_lv;
+% ym_lv = ym - h_lv;
+% ym_sep = ym;
+% ym_rv = ym - h_rv;
+
+% outer lining
+xm_lv = xm_lv - h_lv;
+xm_sep = xm_sep + h_sep;
+xm_rv = xm_rv + h_rv;
+ym_lv = ym + h_lv;
+ym_sep = ym;
+ym_rv = ym + h_rv;
+
+
+Cm_lv=2*xm_lv./max(xm_lv.^2 + ym_lv.^2, 1e-6);
+Cm_sep=2*xm_sep./max(xm_sep.^2 + ym_sep.^2, 1e-6);
+Cm_rv=2*xm_rv./max(xm_rv.^2 + ym_rv.^2, 1e-6);
 
 R_lv = 1./Cm_lv;
 R_sep = 1./Cm_sep;
@@ -29,9 +59,9 @@ x0_lv = xm_lv - R_lv;
 x0_sep = xm_sep - R_sep;
 x0_rv = xm_rv - R_rv;
 
-a_lv = atan2(ym, x0_lv);
-a_sep = atan2(ym, -x0_sep);
-a_rv = atan2(ym, -x0_rv);
+a_lv = atan2(ym_lv, x0_lv);
+a_sep = atan2(ym_sep, -x0_sep);
+a_rv = atan2(ym_rv, -x0_rv);
 
 a_start_lv = 0*pi/2 - a_lv;
 a_stop_lv = 0*pi/2 + a_lv;
@@ -44,8 +74,6 @@ a_stop_rv = 0*pi/2 + a_rv;
 
 plotArc = @(x0, radius,arc)  [radius.*cos(arc) + x0;  radius.*sin(arc)]; 
 
-i = 1;
-figure(101);clf; hold on;
 for i = 1:length(t)
 
     arc_lv = linspace(a_start_lv(i), a_stop_lv(i), 20);
